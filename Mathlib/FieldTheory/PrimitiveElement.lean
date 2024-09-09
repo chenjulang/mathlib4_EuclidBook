b68de00e49242e478a39e494dcb11e7075562317
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-09-09 14:17:55 +0200

    .

--- Mathlib/FieldTheory/PrimitiveElement.lean
+++ Mathlib/FieldTheory/PrimitiveElement.lean
@@ -349,22 +349,17 @@ end Field
 variable (F E : Type*) [Field F] [Field E] [Algebra F E]
     [FiniteDimensional F E] [Algebra.IsSeparable F E]
 
+theorem AlgHom.natCard (K : Type*) [Field K] [IsAlgClosed K] [Algebra F K] :
+    Nat.card (E →ₐ[F] K) = finrank F E :=
+  (AlgHom.natCard_of_powerBasis (L := K) (Field.powerBasisOfFiniteOfSeparable F E)
+    (Algebra.IsSeparable.isSeparable _ _) (IsAlgClosed.splits_codomain _)).trans
+      (PowerBasis.finrank _).symm
+
 @[simp]
 theorem AlgHom.card (K : Type*) [Field K] [IsAlgClosed K] [Algebra F K] :
     Fintype.card (E →ₐ[F] K) = finrank F E := by
-  convert (AlgHom.card_of_powerBasis (L := K) (Field.powerBasisOfFiniteOfSeparable F E)
-    (Algebra.IsSeparable.isSeparable _ _) (IsAlgClosed.splits_codomain _)).trans
-      (PowerBasis.finrank _).symm
+  rw [Fintype.card_eq_nat_card, AlgHom.natCard]
 
-@[simp] -- TODO swap
-theorem AlgHom.natCard (K : Type*) [Field K] [IsAlgClosed K] [Algebra F K] :
-    Nat.card (E →ₐ[F] K) = finrank F E := by
-  convert (AlgHom.card_of_powerBasis (L := K) (Field.powerBasisOfFiniteOfSeparable F E)
-    (Algebra.IsSeparable.isSeparable _ _) (IsAlgClosed.splits_codomain _)).trans
-      (PowerBasis.finrank _).symm
-  rw [Fintype.card_eq_nat_card]
-
-@[simp]
 theorem AlgHom.natCard_of_splits (L : Type*) [Field L] [Algebra F L]
     (hL : ∀ x : E, (minpoly F x).Splits (algebraMap F L)) :
     Nat.card (E →ₐ[F] L) = finrank F E := by
