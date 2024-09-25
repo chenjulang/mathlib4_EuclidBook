21bc2abc91d28302166d178cc59b8991317d5d68
Xavier Roblot <roblot@math.univ-lyon1.fr>
2024-09-25 18:00:49 +0200

    Typo

--- Mathlib/NumberTheory/NumberField/CanonicalEmbedding/Basic.lean
+++ Mathlib/NumberTheory/NumberField/CanonicalEmbedding/Basic.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2022 Xavier Roblot. All rights reserved.t
+Copyright (c) 2022 Xavier Roblot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Xavier Roblot
 -/
