bddac31e56bc4eb197290de3627f0a133ce2b4e8
Xavier Roblot <roblot@math.univ-lyon1.fr>
2024-10-17 11:34:00 +0200

    fix imports

--- Mathlib/NumberTheory/LSeries/Residue.lean
+++ Mathlib/NumberTheory/LSeries/Residue.lean
@@ -3,9 +3,7 @@ Copyright (c) 2024 Xavier Roblot. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Xavier Roblot
 -/
-import Mathlib.NumberTheory.LSeries.Convergence
 import Mathlib.NumberTheory.LSeries.RiemannZeta
-
 
 /-!
 # Docstring
